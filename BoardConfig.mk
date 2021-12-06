2875352aa444ca4538507cc9a639e1b0d6eca7d7
Dylan Neve <dylanneve1@gmail.com>
2021-12-06 23:04:58 +0000

    Initialize crownlte

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,5 +22,9 @@ TARGET_OTA_ASSERT_DEVICE := crownltexx,crownlteks,crownlte
 # Kernel
 TARGET_KERNEL_CONFIG := exynos9810-crownlte_defconfig
 
+# Partitions
+BOARD_VENDORIMAGE_PARTITION_SIZE := 681574400
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
 # Inherit common board flags
 include device/samsung/universal9810-common/BoardConfigCommon.mk
