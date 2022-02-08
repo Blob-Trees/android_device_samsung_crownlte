03820386183818428468db69bd51b0b2a546e617
Cyder | Lumine <87647653+CyderVII28x03@users.noreply.github.com>
2022-02-08 19:32:35 +0700

    Re-config Elixir OSS

--- aosp_crownlte.mk
+++ aosp_crownlte.mk
@@ -25,8 +25,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-Elixir_MAINTAINER := Cyder | Lumine
-CUSTOM_BUILD_TYPE := OFFICIAL_OSS
+Elixir_MAINTAINER := Cyder
+CUSTOM_BUILD_TYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := crownlte
