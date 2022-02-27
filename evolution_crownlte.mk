5459b0a4fd04d2ebda51424d058b0c6cacd7ba5e
Cyder <87647653+CyderVII28x03@users.noreply.github.com>
2022-02-27 22:27:19 +0700

    EvolutionX-Snow bringup.

--- evolution_crownlte.mk
+++ evolution_crownlte.mk
@@ -23,12 +23,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/samsung/crownlte/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+EVO_BUILD_TYPE := UNOFFICIAL
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := crownlte
-PRODUCT_NAME := aosp_crownlte
+PRODUCT_NAME := evolution_crownlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N960F
 PRODUCT_MANUFACTURER := samsung
